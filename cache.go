c764aeff9390708e98a16fa5c5b273bf346870c3
zpatrick <zpatrick319@hotmail.com>
2017-06-15 19:54:23 +0000

    initial commit

---
+++ cache.go
@@ -0,0 +1,104 @@
+package cache
+
+import (
+	"sort"
+	"time"
+)
+
+// A Cache is a thread-safe store for fast item storage retrieval
+type Cache struct {
+	ops chan func(map[string]interface{})
+}
+
+// Returns an empty cache
+func New() *Cache {
+	c := &Cache{
+		ops: make(chan func(map[string]interface{})),
+	}
+
+	go c.loop()
+	return c
+}
+
+func (c *Cache) loop() {
+	items := map[string]interface{}{}
+	for op := range c.ops {
+		op(items)
+	}
+}
+
+// Adds an entry into the cache at the specified key.
+// If an entry already exists at the specified key, it will be overwritten
+func (c *Cache) Add(key string, val interface{}) {
+	c.ops <- func(items map[string]interface{}) {
+		items[key] = val
+	}
+}
+
+// Removes all entries from the Cache
+func (c *Cache) Clear() {
+	c.ops <- func(items map[string]interface{}) {
+		for key := range items {
+			delete(items, key)
+		}
+	}
+}
+
+// Delete an entry from the cache at the specified key.
+// If no entry exists at the specified key, no action is taken
+func (c *Cache) Delete(key string) {
+	c.ops <- func(items map[string]interface{}) {
+		if _, ok := items[key]; ok {
+			delete(items, key)
+		}
+	}
+}
+
+// todo: this doesn't clear entries after N time, clears eveyrtintg after N time
+// should be clear on this
+func (c *Cache) Expire(d time.Duration) *time.Ticker {
+	ticker := time.NewTicker(d)
+	go func() {
+		for range ticker.C {
+			c.Clear()
+		}
+	}()
+
+	return ticker
+}
+
+// Retrieves an entry at the specified key
+func (c *Cache) Get(key string) interface{} {
+	result := make(chan interface{}, 1)
+	c.ops <- func(items map[string]interface{}) {
+		result <- items[key]
+	}
+
+	return <-result
+}
+
+// Retrieves all entry in the cache
+func (c *Cache) Items() map[string]interface{} {
+	result := make(chan map[string]interface{}, 1)
+	c.ops <- func(items map[string]interface{}) {
+		result <- items
+	}
+
+	return <-result
+}
+
+// Retrieves a sorted list of all keys in the cache
+func (c *Cache) Keys() []string {
+	result := make(chan []string, 1)
+	c.ops <- func(items map[string]interface{}) {
+		keys := make([]string, 0, len(items))
+		for k := range items {
+			keys = append(keys, k)
+		}
+
+		sort.Strings(keys)
+		result <- keys
+	}
+
+	return <-result
+}
---
+++ cache_test.go
@@ -0,0 +1,129 @@
+package cache
+
+import (
+	"math/rand"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	c := New()
+	for i := 0; i < 1000; i++ {
+		item := rand.Int()
+		key := strconv.Itoa(item)
+		c.Add(key, item)
+	}
+
+	if length, expected := len(c.Keys()), 1000; length != expected {
+		t.Errorf("Cache had %d keys, expected %d", length, expected)
+	}
+}
+
+func TestAddBasic(t *testing.T) {
+	c := New()
+	c.Add("1", 1)
+
+	if result, expected := c.Get("1"), 1; !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result was %#v, expected %#v", result, expected)
+	}
+}
+
+func TestClear(t *testing.T) {
+	c := New()
+	for i := 0; i < 10; i++ {
+		c.Add(strconv.Itoa(i), i)
+	}
+
+	c.Clear()
+	if length, expected := len(c.Keys()), 0; length != expected {
+		t.Errorf("Cache had %d keys, expected %d", length, expected)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := New()
+	for i := 0; i < 1000; i++ {
+		item := rand.Int()
+		key := strconv.Itoa(item)
+		c.Add(key, item)
+	}
+
+	for _, key := range c.Keys() {
+		c.Delete(key)
+	}
+
+	if length, expected := len(c.Keys()), 0; length != expected {
+		t.Errorf("Cache had %d keys, expected %d", length, expected)
+	}
+}
+
+func TestDeleteBasic(t *testing.T) {
+	c := New()
+	c.Add("1", 1)
+	c.Delete("1")
+
+	if result := c.Get("1"); result != nil {
+		t.Errorf("Result was %#v, expected nil", result)
+	}
+}
+
+func TestExpire(t *testing.T) {
+	c := New()
+	ticker := c.Expire(1)
+
+	for i := 0; i < 10; i++ {
+		c.Add(strconv.Itoa(i), i)
+	}
+
+	// wait for first tick - delayed as the goroutine starts up
+	<-ticker.C
+
+	if length, expected := len(c.Keys()), 0; length != expected {
+		t.Errorf("Cache had %d keys, expected %d", length, expected)
+	}
+}
+
+func TestGet(t *testing.T) {
+	c := New()
+	c.Add("1", 1)
+
+	if result, expected := c.Get("1"), 1; !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result was %#v, expected %#v", result, expected)
+	}
+
+	if result := c.Get("2"); result != nil {
+		t.Errorf("Result was %#v, expected nil", result)
+	}
+}
+
+func TestItems(t *testing.T) {
+	c := New()
+	for i := 0; i < 5; i++ {
+		c.Add(strconv.Itoa(i), i)
+	}
+
+	expected := map[string]interface{}{
+		"0": 0,
+		"1": 1,
+		"2": 2,
+		"3": 3,
+		"4": 4,
+	}
+
+	if result := c.Items(); !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result was %#v, expected %#v", result, expected)
+	}
+}
+
+func TestKeys(t *testing.T) {
+	c := New()
+	for i := 0; i < 5; i++ {
+		c.Add(strconv.Itoa(i), i)
+	}
+
+	expected := []string{"0", "1", "2", "3", "4"}
+	if result := c.Keys(); !reflect.DeepEqual(result, expected) {
+		t.Errorf("Result was %#v, expected %#v", result, expected)
+	}
+}
